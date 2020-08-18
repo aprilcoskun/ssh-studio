401db8e411083fa8ad5907a264174d6ffa901a98
Nisan Coşkun <info@nisancoskun.com>
2020-08-18 14:33:24 +0300

    style: new icons & fonts

--- src/windows/welcome/welcome.js
+++ src/windows/welcome/welcome.js
@@ -1,27 +1,20 @@
-let connections = [];
-const { remote, ipcRenderer } = require('electron');
-const win = remote.getCurrentWindow();
-if (win.connections) {
-  loadConnections(win.connections);
-}
-
-ipcRenderer.on('reload-connection', function (event, connections) {
-  loadConnections(connections);
-});
-
-function loadConnections(conns) {
-  connections = conns;
+const editMsg = 'Edit Window is under development! If you want to change connection options, please delete and create new a connection';
+const { ipcRenderer } = require('electron');
+ipcRenderer.on('reload-connection', function (event, conns) {
+  // connections = conns;
   const box = document.getElementById('connBox');
   let innerHTML =
     '<div style="text-align: center;margin-top:8px">Connections</div><hr>';
   for (let i = 0; i < conns.length; i++) {
-    const label = conns[i].label;
+    const label = conns[i];
+
     innerHTML += `<a class="connection">
-        <div onclick="javascript:ipcRenderer.send('open-connection', '${label}')" class="connection-label">
+        <div onclick="javascript:ipcRenderer.send('open-connection', '${label}')" class="label">
           ${label}
         </div>
-        <div onclick="javascript:ipcRenderer.send('delete-connection', '${label}')" class="tab-close"></div>
+        <div onclick="javascript:alert('${editMsg}')" class="edit"></div>
+        <div onclick="javascript:ipcRenderer.send('delete-connection', '${label}')" class="close"></div>
       </a>`;
   }
   box.innerHTML = innerHTML;
-}
+});
