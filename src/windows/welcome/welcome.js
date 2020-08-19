7c6c07850bcf15889a90fe84162d6c811f3d6448
Nisan Coşkun <info@nisancoskun.com>
2020-08-19 05:39:58 +0300

    chore: updated icons

--- src/windows/welcome/welcome.js
+++ src/windows/welcome/welcome.js
@@ -1,20 +1,14 @@
-const editMsg = 'Edit Window is under development! If you want to change connection options, please delete and create new a connection';
 const { ipcRenderer } = require('electron');
 ipcRenderer.on('reload-connection', function (event, conns) {
-  // connections = conns;
-  const box = document.getElementById('connBox');
   let innerHTML =
     '<div style="text-align: center;margin-top:8px">Connections</div><hr>';
   for (let i = 0; i < conns.length; i++) {
-    const label = conns[i];
-
     innerHTML += `<a class="connection">
-        <div onclick="javascript:ipcRenderer.send('open-connection', '${label}')" class="label">
-          ${label}
-        </div>
-        <div onclick="javascript:alert('${editMsg}')" class="edit"></div>
-        <div onclick="javascript:ipcRenderer.send('delete-connection', '${label}')" class="close"></div>
+        <div onclick="javascript:ipcRenderer.send('open-connection', '${conns[i]}')" class="label">
+          ${conns[i]}
+        </div><div class="space"></div>
+        <div onclick="javascript:ipcRenderer.send('delete-connection', '${conns[i]}')" class="close"></div>
       </a>`;
   }
-  box.innerHTML = innerHTML;
+  document.getElementById('connBox').innerHTML = innerHTML;
 });
